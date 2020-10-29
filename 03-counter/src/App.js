e1ded76a4dad78d59c8a9cb30256f046ae3f44ff
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2020-10-29 21:28:18 +0200

    Styling added

--- 03-counter/src/App.js
+++ 03-counter/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  state = { likes: 0 };
+  state = { likes: 0, color: "#eee" };
 
   //method to add likes
-  addHandler = () => {
+  addHandler = (event) => {
     this.setState({ likes: this.state.likes + 1 });
+    let newColor = this.state.color === "#eee" ? "#ACCEEC" : "#eee";
+    this.setState({ color: newColor });
   };
   //method to remove likes
   removeHandler = () => {
-    this.setState({ likes: this.state.likes - 1 });
+    if (this.state.likes === 0) {
+      return;
+    } else {
+      this.setState({ likes: this.state.likes - 1 });
+    }
   };
   //method for reseting likes
   resetHandler = () => {
@@ -18,11 +24,26 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Total Likes:{this.state.likes}</h1>
-        <button onClick={this.addHandler}>Add one</button>
-        <button onClick={this.removeHandler}>Remove one</button>
-        <button onClick={this.resetHandler}>Reset</button>
+      <div className="App">
+        <h1 className="likes" style={{ backgroundColor: this.state.color }}>
+          Total Likes:{this.state.likes}
+        </h1>
+        <div>
+          <button
+            className="button"
+            onClick={(event) => {
+              this.addHandler(event);
+            }}
+          >
+            Add one
+          </button>
+          <button className="button" onClick={this.removeHandler}>
+            Remove one
+          </button>
+          <button className="button" onClick={this.resetHandler}>
+            Reset
+          </button>
+        </div>
       </div>
     );
   }
