0ee22b60a0ee0d51e364edc5805dedba38925f0d
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2020-10-30 12:07:32 +0200

    School page with functionality to add likes

--- 04-bc-page/src/App.js
+++ 04-bc-page/src/App.js
@@ -1,11 +1,51 @@
 import "./App.css";
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-    </div>
-  );
+class App extends Component {
+  state = { likes: 0 };
+  //method to add likes
+  addHandler = () => {
+    this.setState({ likes: this.state.likes + 1 });
+  };
+
+  refreshPage = () => {
+    this.setState({ likes: 0 });
+  };
+
+  resetHandler = () => {
+    this.setState({ likes: 0 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <main className="main">
+          <div className="cardholder">
+            <img
+              src="http://source.unsplash.com/EhTcC9sYXsw"
+              alt="Computer_img"
+            />
+            <div className="article">
+              <h2>Full Stack Web Developer Program</h2>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Voluptas a, nostrum ex rerum aliquid velit ducimus, ullam
+                repellat, repellendus temporibus quos quidem exercitationem.
+                Totam provident sequi doloribus praesentium aliquam repellendus.
+              </p>
+              <p> {this.state.likes} people like this article</p>
+              <button>Read More</button>
+              <button onClick={this.addHandler}>Give a vote</button>
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ 04-bc-page/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h1>Address</h1>
+    </div>
+  );
+}
+
+export default Footer;
